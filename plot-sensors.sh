015e7f51c41c7712208af94a39d19fbf26e57e16
Jeremy Bobbin <jer@jer.cx>
2021-04-25 08:48:15 -0700

    use raw sensors data instead

--- plot-sensors.sh
+++ plot-sensors.sh
@@ -4,10 +4,10 @@ dir=$(mktemp -d)
 sense() {
 	cd "$dir";
 	# 's' tells us whether the previous line is a section (not empty)
-	sensors | awk '/Adapter|fan/ { next }
+	sensors -u | awk '/Adapter|fan/ { next }
 			/^$/ { s=0; next }
 			!s && !/^$/ { h=$1; s=1; next }
-			/^fan|temp/ { print $2 >> h "-" $1 }'
+			/(fan|temp)[0-9]+_input:/ { sub(/_input:/, "", $1); print $2 >> h "-" $1 }'
 }
 
 plot=$(mktemp -u)
