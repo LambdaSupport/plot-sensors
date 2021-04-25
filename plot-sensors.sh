9b30162d7925c00d9516d8449a1f4aa8203147e9
Jeremy Bobbin <jer@jer.cx>
2021-04-25 08:40:59 -0700

    generate gnu-plot code

--- plot-sensors.sh
+++ plot-sensors.sh
@@ -9,3 +9,31 @@ sense() {
 			!s && !/^$/ { h=$1; s=1; next }
 			/^fan|temp/ { print $2 >> h "-" $1 }'
 }
+
+plot=$(mktemp -u)
+mkfifo "$plot"
+gnuplot -p "$plot" & plotpid=$!
+
+exec > "$plot"
+
+
+cat <<- EOF
+set xdata time
+set timefmt "%M:%S"
+
+set xlabel "Minutes"
+set ylabel "Celcius"
+
+set key left top
+EOF
+
+sense && for file in "$dir"/*; do
+	printf '"%s" using 0:1 with lines title "%s"\n' \
+		"$file" \
+		"$(basename "$file")"
+done | awk 'NR==1 { printf "plot " }
+	NR!=1 { printf ", \\\n\t" }
+	{ printf "%s", $0 }
+	END { printf "\n" }'
+
+wait "$plotpid"
